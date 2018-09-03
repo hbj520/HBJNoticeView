f9c7fc88733bf95e83012bfe50b841c943724d4f
hbj520 <hbjmailformac@163.com>
2018-09-03 10:48:22 +0800

    组件化开发

---
+++ HBJNoticeView/HBJNoticeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HBJNoticeView
+//
+//  Created by Bron on 2018/9/3.
+//  Copyright © 2018 Bron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
